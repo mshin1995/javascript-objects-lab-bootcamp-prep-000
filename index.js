8a59beb6e4e3be3b5ab5c4e0ea70b62a6acfaf51
Matthew Shin <mshin1995@gmail.com>
2019-01-23 05:30:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   return object
 }
 function deleteFromObjectByKey(object, key) {
-  delete object[key]
-  return Object.assign({}, object, key)
+  Object.assign({}, object[key])
+  
 }
 function destructivelyDeleteFromObjectByKey(object, key) {
   delete object[key]
